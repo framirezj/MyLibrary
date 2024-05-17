b7bc5985025954b179aedc994b320b901884dff2
framirezj <12649259+framirezj@users.noreply.github.com>
2024-05-16 20:26:16 -0400

    add directorio de la aplicacion

---
+++ MyLibrary/src/test/java/com/framirezj/MyLibrary/MyLibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.framirezj.MyLibrary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyLibraryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
